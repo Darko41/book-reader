d3f431d5fa49f35b2f56a80b3ffa7891cc6c0a7d
Darko41 <kracunovicd@icloud.com>
2022-05-09 18:40:43 +0200

    Adding welcome page and style CSS

--- src/main/java/com/darko/bookreader/BookReaderApplication.java
+++ src/main/java/com/darko/bookreader/BookReaderApplication.java
@@ -2,9 +2,16 @@ package com.darko.bookreader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookReaderApplication {
+public class BookReaderApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BookReaderApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookReaderApplication.class, args);
---
+++ src/main/java/com/darko/bookreader/controller/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.darko.bookreader.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@Value("${application.message:The books are here!}")
+	private String helloMessage;
+	
+	@GetMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", helloMessage);
+		
+		return "welcome";
+	}
+
+}

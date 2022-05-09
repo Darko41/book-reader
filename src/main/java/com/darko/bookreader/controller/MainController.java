092d2e57daa72de7a0686e104c9972d29f32bcef
Darko41 <kracunovicd@icloud.com>
2022-05-09 18:15:50 +0200

    Working well with getting data

--- src/main/java/com/darko/bookreader/controller/MainController.java
+++ src/main/java/com/darko/bookreader/controller/MainController.java
@@ -2,6 +2,8 @@ package com.darko.bookreader.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +18,7 @@ public class MainController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@PostMapping(path = "/add")
 	public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
 		
 		User user = new User();
@@ -24,6 +27,11 @@ public class MainController {
 		userRepository.save(user);
 		return "Saved";
 		
+	}
+	
+	@GetMapping(path = "/all")
+	public @ResponseBody Iterable<User> getAllUsers() {
+		return userRepository.findAll();
 	}
 
 }

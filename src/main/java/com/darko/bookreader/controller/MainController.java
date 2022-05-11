c2429367050c307cefd05afb83c26f86cd2f42f8
Darko41 <kracunovicd@icloud.com>
2022-05-11 15:26:39 +0200

    Separated UserController methods from MainController

--- src/main/java/com/darko/bookreader/controller/MainController.java
+++ src/main/java/com/darko/bookreader/controller/MainController.java
@@ -3,45 +3,20 @@ package com.darko.bookreader.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.darko.bookreader.entity.Book;
-import com.darko.bookreader.entity.User;
 import com.darko.bookreader.repository.BookDAO;
 import com.darko.bookreader.repository.UserRepository;
 
 @Controller
 //@RequestMapping(path = "/books")
 public class MainController {
-	
-	@Autowired
-	private UserRepository userRepository;
+
 	@Autowired
 	private BookDAO bookDAO;
-	
-	//U S E R S:
-	
-	@PostMapping(path = "/add")
-	public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
-		
-		User user = new User();
-		user.setName(name);
-		user.setEmail(email);
-		userRepository.save(user);
-		return "Saved";
-		
-	}
-	
-	@GetMapping(path = "/all")
-	public @ResponseBody Iterable<User> getAllUsers() {
-		return userRepository.findAll();
-	}
-	
 	
 	//B O O K S:
 	
--- src/test/java/com/darko/bookreader/controller/MainControllerTests.java
+++ src/test/java/com/darko/bookreader/controller/MainControllerTests.java
@@ -1,12 +1,30 @@
 package com.darko.bookreader.controller;
 
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import com.darko.bookreader.entity.Book;
+import com.darko.bookreader.repository.BookDAO;
+
 
 public class MainControllerTests {
 	
 	@InjectMocks
 	private MainController mainController;
 	
+	@Mock
+	private BookDAO bookDAO;
 	
+	@Test
+	public void testGetBookById() {
+		Book b = new Book();
+		b.setId(1);
+		when(bookDAO.getBookById(1)).thenReturn(b);
+		
+		Book book = mainController.get
+	}
 
 }
